21a734f0650c51e7303ec6877a9af29acd0d3bdb
lets travel <george@brewsystems.com>
2020-04-27 14:49:01 -0400

    display issue trying to fix

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-## Badge[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)## Title* Readme-Generator## Description* w## Install* w## Table of Contents* w## Usage* w## License* ISC## Contributions* w## Tests* w## Questions* Profile Pic- https://avatars0.githubusercontent.com/u/60475383?s=460&u=36c6dbdcdb3eea009743535a88f2eb84c6a250ad&v=4* w* Username* - brewtoga* * E-mail Address* - george@brewsystems.com## Badge* https://img.shields.io/github/license/brewtoga/README-generator
+
+    ## Badge[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
+    ## Title
+    * Readme-Generator
+    ## Description
+    * s
+    ## Install
+    * s
+    ## Table of Contents
+    * s
+    ## Usage
+    * s
+    ## License
+    * ISC
+    ## Contributions
+    * s
+    ## Tests
+    * s
+    ## Questions
+    * Profile Pic
+    - https://avatars2.githubusercontent.com/u/60475383?s=460&u=36c6dbdcdb3eea009743535a88f2eb84c6a250ad&v=4
+    * s
+    * Username
+    - brewtoga
+    * E-mail Address
+    - george@brewsystems.com
+    ## Badge* 
+    https://img.shields.io/github/license/brewtoga/README-generator
--- Util/generateMarkdown.js
+++ Util/generateMarkdown.js
@@ -1,9 +1,36 @@
 function generateMarkdown(data) {
-    let profilePic = `https://avatars0.githubusercontent.com/u/60475383?s=460&u=36c6dbdcdb3eea009743535a88f2eb84c6a250ad&v=4`;
+    let profilePic = `https://avatars2.githubusercontent.com/u/60475383?s=460&u=36c6dbdcdb3eea009743535a88f2eb84c6a250ad&v=4`;
     let repoName = "README-generator";
     let badgeHTTPOne = `[![License: ${data.badgeChoice}](https://img.shields.io/badge/License-${data.badgeChoice}-blue.svg)](https://opensource.org/licenses/${data.badgeChoice})`;
-    let badgeHTTP = `https://img.shields.io/github/license/${data.githubUsername}/${repoName}`;
-    return `## Badge${badgeHTTPOne}## Title* ${data.title}## Description* ${data.projectDescription}## Install* ${data.appInstall}## Table of Contents* ${data.tableOfContents}## Usage* ${data.usage}## License* ${data.badgeChoice}## Contributions* ${data.contributing}## Tests* ${data.tests}## Questions* Profile Pic- ${profilePic}* ${data.questions}* Username* - ${data.githubUsername}* * E-mail Address* - ${data.userEmail}## Badge* ${badgeHTTP}`;
+    let badgeHTTP = `
+    https://img.shields.io/github/license/${data.githubUsername}/${repoName}`
+    return`
+    ## Badge${badgeHTTPOne}
+    ## Title
+    * ${data.title}
+    ## Description
+    * ${data.projectDescription}
+    ## Install
+    * ${data.appInstall}
+    ## Table of Contents
+    * ${data.tableOfContents}
+    ## Usage
+    * ${data.usage}
+    ## License
+    * ${data.badgeChoice}
+    ## Contributions
+    * ${data.contributing}
+    ## Tests
+    * ${data.tests}
+    ## Questions
+    * Profile Pic
+    - ${profilePic}
+    * ${data.questions}
+    * Username
+    - ${data.githubUsername}
+    * E-mail Address
+    - ${data.userEmail}
+    ## Badge* ${badgeHTTP}`;
 }
 
 module.exports = generateMarkdown;
