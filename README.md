179c246a860b50d423a252160f20092b5066aa0f
lets travel <george@brewsystems.com>
2020-04-27 15:17:33 -0400

    working on display

--- README.md
+++ README.md
@@ -1,28 +1,27 @@
-
-    ## Badge[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
-    ## Title
-    * Readme-Generator
-    ## Description
-    * s
-    ## Install
-    * s
-    ## Table of Contents
-    * s
-    ## Usage
-    * s
-    ## License
-    * ISC
-    ## Contributions
-    * s
-    ## Tests
-    * s
-    ## Questions
-    * Profile Pic
-    - https://avatars2.githubusercontent.com/u/60475383?s=460&u=36c6dbdcdb3eea009743535a88f2eb84c6a250ad&v=4
-    * s
-    * Username
-    - brewtoga
-    * E-mail Address
-    - george@brewsystems.com
-    ## Badge* 
+## Badge[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
+## Title
+* Readme-Generator]
+## Description
+* e
+## Install
+* e
+## Table of Contents
+* e
+## Usage
+* e
+## License
+* ISC
+## Contributions
+* d
+## Tests
+* d
+## Questions
+* Profile Pic
+- https://avatars2.githubusercontent.com/u/60475383?s=460&u=36c6dbdcdb3eea009743535a88f2eb84c6a250ad&v=4
+* d
+* Username
+- brewtoga
+* E-mail Address
+- george@brewsystems.com
+## Badge* 
     https://img.shields.io/github/license/brewtoga/README-generator
--- Util/generateMarkdown.js
+++ Util/generateMarkdown.js
@@ -4,33 +4,32 @@ function generateMarkdown(data) {
     let badgeHTTPOne = `[![License: ${data.badgeChoice}](https://img.shields.io/badge/License-${data.badgeChoice}-blue.svg)](https://opensource.org/licenses/${data.badgeChoice})`;
     let badgeHTTP = `
     https://img.shields.io/github/license/${data.githubUsername}/${repoName}`
-    return`
-    ## Badge${badgeHTTPOne}
-    ## Title
-    * ${data.title}
-    ## Description
-    * ${data.projectDescription}
-    ## Install
-    * ${data.appInstall}
-    ## Table of Contents
-    * ${data.tableOfContents}
-    ## Usage
-    * ${data.usage}
-    ## License
-    * ${data.badgeChoice}
-    ## Contributions
-    * ${data.contributing}
-    ## Tests
-    * ${data.tests}
-    ## Questions
-    * Profile Pic
-    - ${profilePic}
-    * ${data.questions}
-    * Username
-    - ${data.githubUsername}
-    * E-mail Address
-    - ${data.userEmail}
-    ## Badge* ${badgeHTTP}`;
+    return`## Badge${badgeHTTPOne}
+## Title
+* ${data.title}
+## Description
+* ${data.projectDescription}
+## Install
+* ${data.appInstall}
+## Table of Contents
+* ${data.tableOfContents}
+## Usage
+* ${data.usage}
+## License
+* ${data.badgeChoice}
+## Contributions
+* ${data.contributing}
+## Tests
+* ${data.tests}
+## Questions
+* Profile Pic
+- ${profilePic}
+* ${data.questions}
+* Username
+- ${data.githubUsername}
+* E-mail Address
+- ${data.userEmail}
+## Badge* ${badgeHTTP}`;
 }
 
 module.exports = generateMarkdown;
